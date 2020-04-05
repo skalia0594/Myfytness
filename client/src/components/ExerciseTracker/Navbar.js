26b776311d74b1300379ba93c26963509e61edb0
skalia0594 <skalia@sfu.ca>
2020-04-05 19:20:55 -0400

    mobile test

--- client/src/components/ExerciseTracker/Navbar.js
+++ client/src/components/ExerciseTracker/Navbar.js
@@ -3,16 +3,24 @@ import {Link} from 'react-router-dom';
 
 function Navbar(){
     return(
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">MY FITNESS</Link>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="navbar-item"><Link className="nav-link" to="/">Excercises</Link></li>
-                        <li className="navbar-item"><Link className="nav-link" to="/create">Create Exercise Log</Link></li>
-                        <li className="navbar-item"><Link className="nav-link" to="/user">Create User</Link></li>
-                    </ul>
-               </div>     
+    
+            <div className="navbar-collapse collapse">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Excercises</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/create">Create Exercise Log</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/user">Create User</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
+
     );
 }
 export default Navbar
