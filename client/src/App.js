a2c356fbdbf171a027bd55444077ea2e8b81c41c
skalia0594 <skalia@sfu.ca>
2020-04-05 19:26:27 -0400

    mobile test

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App(){
     return(
         // <h1>Hello</h1>
         <Router>
-            <div className="container">
+            <div>
                 <Navbar />
                 <br />
                 <Switch>
